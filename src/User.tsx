2993f231f80fc124b20bfa7076bf2733cfe33240
byeongwoonlee <tfh100@gmail.com>
2022-05-11 09:06:30 +0900

    스테이징 테스트

--- src/User.tsx
+++ src/User.tsx
@@ -1,27 +1,27 @@
 import React, { useEffect } from "react";
 import { UserType } from "./type";
 import { useUsersState, useUsersDispatch, getUser } from "./UsersContext";
-
+//gitTest
 function User({ id }: UserType) {
-  const state = useUsersState();
-  const dispatch = useUsersDispatch();
+    const state = useUsersState();
+    const dispatch = useUsersDispatch();
 
-  useEffect(() => {
-    getUser(dispatch, id);
-  }, [dispatch, id]);
-  const { loading, data: user, error } = state.user;
-  if (loading) return <div>로딩중..</div>;
-  if (error) return <div>에러가 발생햇습니다..</div>;
-  if (!user) return null;
-  return (
-    <div>
-      <h2>{user.username}</h2>
-      <p>
-        <b>Email:</b>
-        {user.email}
-      </p>
-    </div>
-  );
+    useEffect(() => {
+        getUser(dispatch, id);
+    }, [dispatch, id]);
+    const { loading, data: user, error } = state.user;
+    if (loading) return <div>로딩중..</div>;
+    if (error) return <div>에러가 발생햇습니다..</div>;
+    if (!user) return null;
+    return (
+        <div>
+            <h2>{user.username}</h2>
+            <p>
+                <b>Email:</b>
+                {user.email}
+            </p>
+        </div>
+    );
 }
 
 export default User;
